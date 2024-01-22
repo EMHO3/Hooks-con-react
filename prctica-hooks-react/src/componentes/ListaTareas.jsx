63a453603b8ff83bf2a318196c1480466bf9f09e
Emir Huaroc <emirmho3@gmail.com>
2024-01-22 17:40:09 -0500

    UseReducer

--- prctica-hooks-react/src/componentes/ListaTareas.jsx
+++ prctica-hooks-react/src/componentes/ListaTareas.jsx
@@ -1,37 +1,87 @@
+import { useForm } from "../hooks/useForm"
+import { useReducer } from "react"
+
 const initialState=[{
-    id:1,
+    id:new Date().getDate(),
     tarea:'Explicar Reducers',
     finalizada:false
 }]
-const nuevaTarea={
-    id:2,
-    tarea:'Explicar useReducer',
-    finalizada:false
-}
-const agregarTarea={
-    type:"[Tareas] Agregar tarea",
+
+const editarTarea={
+    tarea:'[TAREA] Editar tarea',
     payload:nuevaTarea
 }
+const eliminarTarea={
+    tarea:'[TAREA] Edliminar tarea',
+}
+const borrarTarea={
+    tarea:'[TAREA] Borrar tarea',
+}
+
 
 const tareaReducer=(state=initialState,action={})=>{
-    if(action.type==="[Tareas] Agregar tarea"){
-        return[...state,action.payload]
+    switch (action.type) {
+        case '[TAREAS] Agregar tarea':
+            return[...state,action.payload]
+        case '[TAREAS] Editar tarea':
+            console.log("editar")
+        case '[TAREAS] Eliminar tarea':
+            console.log("eliminar")
+        case '[TAREAS] Borrar tarea':
+            console.log("borrar todo")
+            return[]            
+        default:
+            return state
+
     }
-    return state
 }
-console.log(tareaReducer(initialState,agregarTarea))
-
-
-
-
-
-
 
 
 
 export const ListaTareas = () => {
+    const [tareasState, dispatch] = useReducer(tareaReducer, initialState)
+    const{tarea,formState,onInputChange}= useForm({tarea:''})
+    const agregarTarea=(event)=>{
+        event.preventDefault()
+        if(formState.value=='')return
+        console.log(formState)
+        const tarea={
+            id:new Date().getTime(),
+            tarea:formState.tarea,
+            finalizada:false
+        }
+        const action={
+            type:'[TAREAS] Agregar tarea',
+            payload:tarea
+        }
+        dispatch(action)
+    }
   return (
     <>
+       <form onSubmit={agregarTarea}>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              name="tarea"  
+              placeholder="Ingrese tarea"
+              onChange={onInputChange}
+              value={tarea}
+            />
+          </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <hr/>
+        <ul>
+        {tareasState.map(item=>{
+            return(
+                <li key={item.id}>
+                    {item.tarea}
+                    {item.finalizada ?"✅":"❌"}</li>
+            )
+        })}
+        </ul>
+
     </>
   )
 }
